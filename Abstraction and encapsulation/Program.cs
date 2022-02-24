8b84b42be902a1d1a19f704fd2f3e5e42ffa583f
Dell <Dell@HARRY>
2022-02-24 09:20:18 +0545

    Add project files.

---
+++ Abstraction and encapsulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstraction_and_encapsulation
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
